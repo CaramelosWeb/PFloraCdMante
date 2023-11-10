55ef0c288e2433e2fabe10e25003bc21829ec8b5
Sami Balleza Mtz <balleza.sami.19@gmail.com>
2023-11-09 21:29:07 -0600

    Se agrego PFlora

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Flora De Cd.Mante
+    </title>
+    <link rel="stylesheet" href="./css/style.css"> 
+</head>
+<body>
+
+       <!-- uso de texto en contenedor -->
+       <center>
+        
+  <div class="container-fluid p-5 color-modes">
+    <div class="row justify-content-center">
+        <div class="col">
+            <h1 class="text-center">Flora De Cd.Mante</h1>
+        </div>
+    </div>
+        
+        <!-- Termino de contenedor -->
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/oJEuOi-SFW0?si=Joi7cSziAIky-E1C" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+            </center>
+</body>
+</html>
